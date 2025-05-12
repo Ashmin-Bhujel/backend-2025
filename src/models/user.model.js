0c3fb90ec3f995cf9ddeb0ea10d8492cea9a359c
Ashmin Bhujel <ashminbhujel01@gmail.com>
2025-05-12 17:28:29 +0545

    add: User and video schema

---
+++ src/models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: [true, "Fullname is required"],
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // Cloudinary URL
+      required: true,
+    },
+    coverImage: {
+      type: String, // Clodinary URL
+    },
+    watchHistory: [
+      {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      trim: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
