a91de8a140d07e11efd93714b543a607827b4758
Ashmin Bhujel <ashminbhujel01@gmail.com>
2025-05-26 18:33:23 +0545

    feat: Update HTTP methods for some routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -40,22 +40,24 @@ router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-access-token").post(refreshAccessToken);
 
 // Change current password
-router.route("/change-current-password").post(verifyJWT, changeCurrentPassword);
+router
+  .route("/change-current-password")
+  .patch(verifyJWT, changeCurrentPassword);
 
 // Get current user
 router.route("/get-current-user").get(verifyJWT, getCurrentUser);
 
 // Update user data
-router.route("/update-user-data").post(verifyJWT, updateUserData);
+router.route("/update-user-data").patch(verifyJWT, updateUserData);
 
 // Update avatar image
 router
   .route("/update-avatar-image")
-  .post(verifyJWT, upload.single("avatar"), updateAvatarImage);
+  .patch(verifyJWT, upload.single("avatar"), updateAvatarImage);
 
 // Update cover image
 router
   .route("/update-cover-image")
-  .post(verifyJWT, upload.single("coverImage"), updateCoverImage);
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
 
 export default router;
